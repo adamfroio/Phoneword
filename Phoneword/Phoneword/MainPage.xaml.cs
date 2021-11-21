81c1ff50552c090195e22e8eb013d6e4c93d890c
Adam Froio <afroio@gmail.com>
2021-11-21 11:33:30 -0800

    Added controls

--- Phoneword/Phoneword/MainPage.xaml.cs
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Phoneword
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+
         public MainPage()
         {
-            InitializeComponent();
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label { 
+                Text = "Enter a Phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false
+            });
+
+            this.Content = panel;
+
+            //InitializeComponent();
         }
     }
 }
